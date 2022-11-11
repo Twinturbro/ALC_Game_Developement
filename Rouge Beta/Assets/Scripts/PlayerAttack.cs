537d1ef435575efa52bf1585a3e85ca961a04c3a
Twinturbro <tanner.m212@stu.nebo.edu>
2022-11-11 09:19:31 -0700

    added player attack to player and player health/death
    player can dow kill or be killed

--- Rouge Beta/Assets/Scripts/PlayerAttack.cs
+++ Rouge Beta/Assets/Scripts/PlayerAttack.cs
@@ -6,23 +6,24 @@ public class PlayerAttack : MonoBehaviour
 {
     private float attackDelay;
     public float startDelay;
-    public Transform attackpos;
+    public Transform attackPos;
     public LayerMask whatIsEnemies;
     public float attackRange;
     public int damage;
+
 
     // Update is called once per frame
     void Update()
     {
         if (attackDelay <= 0)
         {
-            if (Input.GetKey(KeyCode.Space))
+            if (Input.GetKey(KeyCode.F))
             {
-                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackpos.position, attackRange, whatIsEnemies);
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
 
                 for (int i = 0; i < enemiesToDamage.Length; i++)
                 {
-                    enemiesToDamage[i].GetComponent < EnemyHealth>().TakeDamage(damage);
+                    enemiesToDamage[i].GetComponent<EnemyHealth>().TakeDamage(damage);
                 }
 
                 attackDelay = startDelay;
@@ -32,5 +33,11 @@ public class PlayerAttack : MonoBehaviour
         {
             attackDelay -= Time.deltaTime;
         }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
     }
 }
